6fca8bf4c6b52594a3a2cf4e535accd4d9931d25
majodev <contact@majodev.com>
2013-10-17 22:41:57 +0200

    show projection in debug

--- src/lib/base/leapManager.js
+++ src/lib/base/leapManager.js
@@ -225,7 +225,9 @@ define(["log", "Leap", "appConfig", "utils/publisher", "Poll", "gameConfig",
         interval: 2000,
         action: function() {
           var time = frameCount / 2,
-            debugText = "leap @ " + time + "fps";
+            debugText = "leap @ " + time + "fps\n";
+            debugText += "projection @ " + Math.round(getProjectionSizeInMillimeters().width) +
+            " mm x " + Math.round(getProjectionSizeInMillimeters().height) + " mm";
 
           if (attachedListeners === false) {
             debugText = "no leap connected.";
